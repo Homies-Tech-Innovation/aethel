bc4ce5114dcb5c96ca555a5e634072b76c0190c2
DiamondOsas <diamondosazuwa@gmail.com>
2025-10-17 20:16:39 +0100

    Error Handling System - ApiError Classes

---
+++ backend/aethel-backend/src/utils/errors/ApiError.ts
@@ -0,0 +1,17 @@
+export class ApiError extends Error {
+  public statusCode: number;
+  public success: boolean;
+  public errors: string[];
+
+  constructor(
+    statusCode: number,
+    message: string = "Something went wrong", //just in case no special error can represent the case
+    errors: string[] = []
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.success = false;
+    this.errors = errors;
+  }
+}
---
+++ backend/aethel-backend/src/utils/errors/index.ts
@@ -0,0 +1,12 @@
+//Tested 
+export {ApiError} from "./ApiError";
+
+export {
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+  InternalServerError,
+} from "./SpecialErrors";
+
